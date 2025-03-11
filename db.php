a00090b4afa02007b8cfd1dd8a2c5103a76f52d6
Ivan <84211523+ivan-the-noob@users.noreply.github.com>
2025-03-11 20:47:52 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
-// $servername = "localhost";
-// $username = "u373116035_digitalpaws";
-// $password = "#Bakitako23";
-// $dbname = "u373116035_digitalpaws";
-
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "paws";
+$username = "u373116035_digitalpaws";
+$password = "#Bakitako23";
+$dbname = "u373116035_digitalpaws";
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "paws";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
