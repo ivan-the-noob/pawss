6be0bb105785b80369e21a6b354a3c4f234aad33
Miles <MilesBriones@cvsu.edu.ph>
2025-04-28 00:53:38 +0800

    dasdsa

--- features/admin/function/php/approve_to-receive.php
+++ features/admin/function/php/approve_to-receive.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $notificationStmt->execute();
             $notificationStmt->close();
             
-            header("Location: ../../web/api/to-ship.php");
+            header("Location: ../../web/api/to-ship_checkout.php");
             exit;  
         } else {
             header("Location: ../../view_orders.php?message=Error occurred while approving the orders.");
