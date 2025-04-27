af371ce0baf4db0201f86f9c9cee600674f2a4d1
Miles <MilesBriones@cvsu.edu.ph>
2025-04-28 01:14:49 +0800

    dadsa

--- features/admin/function/script/order.js
+++ features/admin/function/script/order.js
@@ -1,131 +1,137 @@
-function submitDecline() {
-    var form = document.getElementById('appointmentForm');
-    var id = document.getElementById('modalUserId').value; 
 
-    form.action = '../../function/php/delete_appointment.php'; 
-    form.method = 'POST';
+    function submitDecline() {
+        var form = document.getElementById('appointmentForm');
+        var id = document.getElementById('modalUserId').value; 
 
-    var idInput = document.createElement("input");
-    idInput.type = "hidden";
-    idInput.name = "id"; 
-    idInput.value = id;
-    form.appendChild(idInput);
+        form.action = '../../function/php/delete_appointment.php'; 
+        form.method = 'POST';
 
-    form.submit();
-}
+        var idInput = document.createElement("input");
+        idInput.type = "hidden";
+        idInput.name = "id"; 
+        idInput.value = id;
+        form.appendChild(idInput);
 
+        form.submit();
+    }
 
-$('#viewModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget); 
-    var modal = $(this);
+    $('#viewModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); 
+        var modal = $(this);
 
-    var id = button.data('id');
-    var name = button.data('name');
-    var email = button.data('email');
-    var contactNum = button.data('contact-num');
-    var addressSearch = button.data('address-search');
-    var paymentMethod = button.data('payment-method');
-    var screenshot = button.data('screenshot');
-    var referenceId = button.data('reference_id');
-    var shippingFee = button.data('shipping-fee');
-    var totalAmount = button.data('total-amount');
-    var status = button.data('status');
-    var products = button.data('products');
+        var id = button.data('id');
+        var name = button.data('name');
+        var email = button.data('email');
+        var contactNum = button.data('contact-num');
+        var addressSearch = button.data('address-search');
+        var paymentMethod = button.data('payment-method');
+        var screenshot = button.data('screenshot');
+        var referenceId = button.data('reference_id');
+        var shippingFee = button.data('shipping-fee');
+        var totalAmount = button.data('total-amount');
+        var status = button.data('status');
+        var products = button.data('products');
 
-    var latitude = button.data('latitude');
-    var longitude = button.data('longitude');
+        var latitude = button.data('latitude');
+        var longitude = button.data('longitude');
 
-    var totalSubTotal = 0; 
-    var totalQuantity = 0;  
-    var productCardHtml = '';  
+        var totalSubTotal = 0; 
+        var totalQuantity = 0;  
+        var productCardHtml = '';  
 
-    products.forEach(function (product) {
-        totalSubTotal += parseFloat(product.sub_total); 
-        totalQuantity += product.quantity; 
-        // Ensure the correct image path is being set
-        var productImagePath = `../../../../assets/img/product/${product.product_img}`;
+        products.forEach(function (product) {
+            totalSubTotal += parseFloat(product.sub_total); 
+            totalQuantity += product.quantity; 
+            productCardHtml += `
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="../../../../assets/img/product/${product.product_img}" class="img-fluid" alt="${product.product_name}">
+                    </div>
+                    <div class="col-md-8">
+                        <h5 class="card-title">${product.product_name}</h5>
+                        <div class="d-flex justify-content-between">
+                            <p class="card-text">Qty: ${product.quantity}x</p>
+                            <p class="card-text">₱${parseFloat(product.sub_total).toFixed(2)}</p>
+                       </div>
+                    </div>
+                </div>
+                <hr>
+            `;
+        });
 
         productCardHtml += `
             <div class="row">
-                <div class="col-md-4">
-                    <img src="${productImagePath}" class="img-fluid" alt="${product.product_name}">
-                </div>
-                <div class="col-md-8">
-                    <h5 class="card-title">${product.product_name}</h5>
-                    <div class="d-flex justify-content-between">
-                        <p class="card-text">Qty: ${product.quantity}x</p>
-                        <p class="card-text">₱${parseFloat(product.sub_total).toFixed(2)}</p>
-                   </div>
+                <div class="col-md-12">
+                   <p class="card-text d-flex justify-content-end text-align-start"><strong>Shipping Fee:</strong> Via Lalamove</p>
+                   <p class="card-text d-flex justify-content-end text-align-start"><strong>Total Amount:</strong> ₱${totalAmount.toFixed(2)}</p>
                 </div>
             </div>
-            <hr>
         `;
+
+        modal.find('#modalId').val(id);
+        modal.find('#modalName').val(name);
+        modal.find('#modalEmail').val(email);
+        modal.find('#modalContactNum').val(contactNum);
+        modal.find('#modalAddressSearch').val(addressSearch);
+        modal.find('#modalPaymentMethod').val(paymentMethod);
+        
+        var screenshotPath = "../../../../assets/img/check-out/" + screenshot; 
+        modal.find('#modalScreenshot').attr('src', screenshotPath);
+        modal.find('#modalReferenceId').val(referenceId);
+        modal.find('#modalShippingFee').val(shippingFee);
+        modal.find('#modalTotalAmount').val(totalAmount);
+        modal.find('#modalStatus').val(status);
+
+        modal.find('#hiddenId').val(id);
+        modal.find('#hiddenName').val(name);
+        modal.find('#hiddenEmail').val(email);
+        modal.find('#hiddenContactNum').val(contactNum);
+        modal.find('#hiddenAddressSearch').val(addressSearch);
+        modal.find('#hiddenPaymentMethod').val(paymentMethod);
+        modal.find('#hiddenScreenshot').val(screenshot);
+        modal.find('#hiddenReferenceId').val(referenceId);
+        modal.find('#hiddenShippingFee').val(shippingFee);
+        modal.find('#hiddenTotalAmount').val(totalAmount);
+        modal.find('#hiddenStatus').val(status);
+
+        modal.find('#productCards').html(productCardHtml);
+
+        if (latitude && longitude) {
+            initMap(latitude, longitude); 
+        }
     });
 
-    productCardHtml += `
-        <div class="row">
-            <div class="col-md-12">
-               <p class="card-text d-flex justify-content-end text-align-start"><strong>Shipping Fee:</strong> Via Lalamove</p>
-               <p class="card-text d-flex justify-content-end text-align-start"><strong>Total Amount:</strong> ₱${totalAmount.toFixed(2)}</p>
-            </div>
-        </div>
-    `;
+    function initMap(lat, lng) {
+        var mapOptions = {
+            center: new google.maps.LatLng(lat, lng),
+            zoom: 18, 
+            mapTypeId: google.maps.MapTypeId.ROADMAP 
+        };
 
-    // Update modal fields with the gathered data
-    modal.find('#modalId').val(id);
-    modal.find('#modalName').val(name);
-    modal.find('#modalEmail').val(email);
-    modal.find('#modalContactNum').val(contactNum);
-    modal.find('#modalAddressSearch').val(addressSearch);
-    modal.find('#modalPaymentMethod').val(paymentMethod);
-    
-    var screenshotPath = "../../../../assets/img/check-out/" + screenshot; 
-    modal.find('#modalScreenshot').attr('src', screenshotPath);
-    modal.find('#modalReferenceId').val(referenceId);
-    modal.find('#modalShippingFee').val(shippingFee);
-    modal.find('#modalTotalAmount').val(totalAmount);
-    modal.find('#modalStatus').val(status);
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    // Set the product cards HTML in the modal
-    modal.find('#productCards').html(productCardHtml);
-
-    if (latitude && longitude) {
-        initMap(latitude, longitude); 
+        var marker = new google.maps.Marker({
+            position: {lat: parseFloat(lat), lng: parseFloat(lng)},
+            map: map,
+            title: 'Appointment Location'
+        });
     }
-});
 
-
-
-function initMap(lat, lng) {
-    var mapOptions = {
-        center: new google.maps.LatLng(lat, lng),
-        zoom: 18, 
-        mapTypeId: google.maps.MapTypeId.ROADMAP 
-    };
-
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: {lat: parseFloat(lat), lng: parseFloat(lng)},
-        map: map,
-        title: 'Appointment Location'
-    });
-}
-
-$('#viewModal').on('shown.bs.modal', function() {
-    var modal = $(this);
-    modal.find('#declineButton').off('click').on('click', function() {
-        $.ajax({
-            url: '../../function/php/delete_user.php', 
-            type: 'POST',
-            data: { user_id: modal.find('#modalId').val() },
-            success: function(response) {
-                alert('Appointment declined and deleted.');
-                location.reload(); 
-            },
-            error: function() {
-                alert('Error occurred while declining the appointment.');
-            }
+    $('#viewModal').on('shown.bs.modal', function() {
+        var modal = $(this);
+        modal.find('#declineButton').off('click').on('click', function() {
+            $.ajax({
+                url: '../../function/php/delete_user.php', 
+                type: 'POST',
+                data: { user_id: modal.find('#modalId').val() },
+                success: function(response) {
+                    alert('Appointment declined and deleted.');
+                    location.reload(); 
+                },
+                error: function() {
+                    alert('Error occurred while declining the appointment.');
+                }
+            });
         });
     });
-});
+
